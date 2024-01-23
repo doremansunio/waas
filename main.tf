4f6d01713f319d9064b039cef99ee2b584532c3b
Phani <Phani@gmail.com>
2024-01-23 11:41:37 +0530

    Commit 1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "rafay" {
-  # Configuration options
+  # Configuration option
   profile = ""
   api_key =  "ra2.32ce1e1f34e3787837b3738366547557b9cf90b9.d4d73fd4942d9587616c10c22ce9e4b2d439628dcc19ff59d42be9195aeb9ba6"
   api_secret = "d4d73fd4942d9587616c10c22ce9e4b2d439628dcc19ff59d42be9195aeb9ba6"
