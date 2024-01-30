bfdc74393e6330deb192d3ebe0a5ec769db931d3
Phani <Phani@gmail.com>
2024-01-30 15:47:59 +0530

    a8

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "network_policy_rule_name" {
 
 variable "network_policy_rule_filepath" {
   type = string
-  default = "files://myrepo/withinworkspace-policy-rule.yaml"
+  default = "files://myrepo/test.yaml"
 }
 
 variable "network_policy_rule_version" {
