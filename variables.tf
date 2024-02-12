e445057f53957187555effd9c615f0cfe3dbcb97
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-02-12 17:01:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,39 @@
+variable "central_pool_name" {
+  type = string
+  default = "centralpool"
+}
 
+variable "project_name" {
+  type = string
+  default = "team-q"
+} 
+
+variable "cluster_name" {
+  type = string
+  default = "multi-eks1"
+}
+
+variable "workspace_admins" {
+  type    = string
+  default = "phani.kg@gmail.com"
+}
+
+variable "network_policy_name" {
+  type = string
+  default = "deny-all-for-ns-policy"
+}
+
+variable "network_policy_rule_name" {
+  type = string
+  default = "deny-all-for-ns-policy-rule"
+}
+
+variable "network_policy_rule_filepath" {
+  type = string
+  default = "file://deny-all-for-namespace.yaml"
+}
+
+variable "network_policy_rule_version" {
+  type = string
+  default = "v1"
+}
